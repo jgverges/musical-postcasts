06491e80df028260d3a6354ab5549f1aacf9e671
jgverges <jgverges@yahoo.es>
2024-03-26 20:53:44 +0100

    feat(webpack): Implement code splitting for better bundle optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,73 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
-  const isProduction = argv.mode === 'production';
+  const isProduction = argv.mode === "production";
 
   return {
-    entry: './src/index.tsx',
+    entry: "./src/index.tsx",
     output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: isProduction ? 'bundle.min.js' : 'bundle.js',
-      publicPath: '/',
+      path: path.resolve(__dirname, "dist"),
+      filename: isProduction ? "[name].bundle.min.js" : "[name].bundle.js",
+      publicPath: "/",
     },
     devServer: {
       static: {
-        directory: path.join(__dirname, 'public'),
+        directory: path.join(__dirname, "public"),
       },
       port: 3000,
-      historyApiFallback: true 
+      historyApiFallback: true,
     },
     resolve: {
-      extensions: ['.tsx', '.ts', '.js'],
+      extensions: [".tsx", ".ts", ".js"],
     },
     module: {
       rules: [
         {
           test: /\.(ts|tsx)$/,
-          use: 'ts-loader',
+          use: "ts-loader",
           exclude: /node_modules/,
         },
         {
           test: /\.css$/i,
           use: [
-            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
-            'css-loader',
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
           ],
         },
         {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
+          type: "asset/resource",
         },
-        
       ],
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './public/index.html',
+        template: "./public/index.html",
       }),
-      isProduction && new MiniCssExtractPlugin({
-        filename: 'styles.min.css',
-      }),
+      isProduction &&
+        new MiniCssExtractPlugin({
+          filename: "styles.min.css",
+        }),
     ].filter(Boolean),
     optimization: {
       minimize: isProduction,
-      minimizer: [
-        new TerserPlugin(),
-      ],
+      minimizer: [new TerserPlugin()],
+      splitChunks: {
+        chunks: "all",
+        cacheGroups: {
+          default: false,
+          vendors: false,
+          customGroup: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "customGroup",
+            chunks: "all",
+            priority: 10,
+          },
+        },
+      },
     },
   };
 };
