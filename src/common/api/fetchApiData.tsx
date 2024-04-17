ae8b935e5781179fc324b3c618e6d121f2d86b8e
jgverges <jgverges@yahoo.es>
2024-04-17 10:55:25 +0200

    feat: Add error handling for fetch request when response is not ok

--- src/common/api/fetchApiData.tsx
+++ src/common/api/fetchApiData.tsx
@@ -2,6 +2,7 @@ export async function fetchApiData(url: string, signal: AbortSignal) {
   try {
     const response = await fetch(url, { signal });
     if (signal?.aborted) return null;
+    if (!response.ok) throw new Error("Error fetching data");
     return response.json();
   } catch (error) {
     if (signal?.aborted) {
